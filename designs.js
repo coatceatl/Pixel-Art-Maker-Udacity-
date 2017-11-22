efd8d2f9aea3cbdb1f56d95872bccfa84ec9485a
Oksana Tkachenko <oksana@crystax.net>
2017-11-22 15:27:14 +0200

    added function makeGrid

--- designs.js
+++ designs.js
@@ -6,30 +6,33 @@ const sizePicker = $('#sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
+function makeGrid() {
+  // Your code goes here!
 
-sizePicker.submit(function(event) {
   $('#pixel_canvas').empty();
-  event.preventDefault();
   const height = $('#input_height').val();
   const width = $('#input_width').val();
 
   for(let i = 0; i < height; i++) {
-    $('#pixel_canvas').append('<tr class="row' + i + '"></tr>');
+    $('#pixel_canvas').append('<tr id="row' + i + '"></tr>');
 
+    // Usually, it makes sense to use 'for' loop here too.
+    // However, there was a requirement to demonstrate both 'for' and 'while' loops.
+    // That's why 'while' is used here.
     let j = 0;
     while(j < width) {
-      $('.row' + i).append('<td></td>');
+      $('#row' + i).append('<td></td>');
       j++;
     }
   }
+}
+
+sizePicker.submit(function(event) {
+  event.preventDefault();
+  makeGrid();
 });
 
-$('#pixel_canvas').on('click','td', (event) => {
+$('#pixel_canvas').on('click','td', function(event) {
   const color = colorPicker.val();
   $(event.currentTarget).css('background', color);
 });
-
-
-// Your code goes here!
-
-
